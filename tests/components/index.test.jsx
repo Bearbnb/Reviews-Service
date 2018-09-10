064e3c4fc53033994401c28caa7426ca15049187
Celeste Zamora <cels.zam@gmail.com>
2018-09-10 16:53:33 -0700

    Set up Testing

--- tests/components/index.test.jsx
+++ tests/components/index.test.jsx
@@ -1,10 +1,11 @@
 import { shallow, mount, render } from 'enzyme';
-import React from 'react';
 import toJson from 'enzyme-to-json';
+import React from 'react';
 import App from '../../client/components/index';
 
-it('App should render to the DOM', () => {
-  const wrapper = shallow(<App />);
-  expect(toJson(wrapper)).toMatchSnapshot();
-});
-
+describe('Index Component', ()=>{
+	it('Index should render App to the DOM', () => {
+	  const wrapper = shallow(<App />);
+	  expect(toJson(wrapper)).toMatchSnapshot();
+	});
+})
