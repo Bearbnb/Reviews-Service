af651c277f6b7243c7b9ee9096488a6dd8e17059
Celeste Zamora <cels.zam@gmail.com>
2018-09-12 00:03:32 -0700

    Working on styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,4 @@ module.exports = {
     "ecmaFeatures": {
       "jsx": true
     }
-  },
   };
--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from '../../styles/Review.css';
 const moment = require('moment')
+
 let Review = (props) => {
     return (
-        <div className={styles.review}>
-            <div className="media-left media-middle">
+        <div className={styles.reviewContainer}>
+            <div className={styles.header}>
                 <img className={styles.mediaobject} src={props.review.photo} alt="" />
-            </div>
-            <div className="reviewer-name">
-                <div className="review-writer">
+                <div>
+                    <div className={styles.reviewDate}>
                     {moment(props.review.created).format("MMM Do YY")}
+                    </div>
+                    <div className={styles.reviewer}>
+                    {props.review.name}
+                    </div>
                 </div>
-                <div className="video-list-entry-detail">
-                    {props.review.review}
-                </div>
+            </div>
+            <div className={styles.reviewText}>
+                <p>{props.review.review}</p>
+            </div>
+            <div className={styles.bottom}>
+                <div></div>
             </div>
         </div>
     );
